920af648955311bd88ca4ea24bb0a559c9ebad2d
Kxllve <coreyprescott416@protonmail.com>
2026-08-06 22:54:30 +0100

    Added code and updated README.md.

---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PasswordGenerator
+{
+    class Program
+    {
+        private const string CHAR_POOL = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!$%&*@#";
+        private const int DEFAULT_LENGTH = 16;
+        static void Main(string[] args)
+        {
+            int passwordLength = 16;
+
+            Console.WriteLine("Welcome to the Password Generator!");
+            Console.Write("Please enter a length (default: 16 characters): ");
+            if (int.TryParse(Console.ReadLine(), out passwordLength)){
+                GeneratePassword(passwordLength);
+            }
+        }
+
+        static void GeneratePassword(int passwordLength)
+        {
+            if (passwordLength < 2)
+            {
+                passwordLength = DEFAULT_LENGTH;
+            }
+
+            using var rng = RandomNumberGenerator.Create();
+            StringBuilder password = new StringBuilder();
+
+            byte[] randomBytes = new byte[passwordLength];
+            rng.GetBytes(randomBytes);
+
+            for (int i = 0; i < passwordLength; i++)
+            {
+                int currentByte = randomBytes[i] % 69;
+                char currentChar = CHAR_POOL[currentByte];
+                password.Append(currentChar);
+            }
+
+            Console.WriteLine(password);
+            TextCopy.ClipboardService.SetText(password.ToString());
+            Console.WriteLine("Copied the password to your clipboard.");
+            Console.WriteLine("WARNING: Clipboard is not secure. Other apps can read it!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Password-Generator-CSharp
-A simple, password generator written in C#
+A simple, secure password generator written in C#.
+
+This program will only work on Linux.
+
+### Prerequisites
+
+- .NET SDK 8.0 or higher
+- Linux: `xsel` installed (Arch: `sudo pacman -S xsel`)
+
+### Installation
+
+Clone or download this repository:
